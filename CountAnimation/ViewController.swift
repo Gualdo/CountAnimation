406966b5497eddf9af5401ff83ec5a8c1f354466
Eduardo De La Cruz <gualdodelacruz@gmail.com>
2018-11-27 18:10:45 +0100

    Minor change

--- CountAnimation/ViewController.swift
+++ CountAnimation/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
     var startValueForString = 0
     let endValue: Double = 1000
     let animationDuration: Double = 6.5
-    var startText = " "
+    var startText = ""
     let endText: String = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged."
     
     let animationStartDate = Date()
